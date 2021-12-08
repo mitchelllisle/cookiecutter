2589866b9b2a80a19da08b40b5d3e037d2ae8e1a
Mitchell Lisle <lislemitchell@gmail.com>
2021-12-08 11:27:12 +1100

    change tests

--- {{cookiecutter.project_slug}}/tests/test_version.py
+++ {{cookiecutter.project_slug}}/tests/test_version.py
@@ -2,5 +2,5 @@ import re
 from {{cookiecutter.project_slug}} import __version__
 
 def test_version():
-    match = re.search('\d\.\d.\d$', __version__)
+    match = re.search(r'^\d+\.\d+\.\d+$', __version__)
     assert match is not None
