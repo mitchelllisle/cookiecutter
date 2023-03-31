f445d9acbde574465a938f6ea5201db00a6fb72f
Mitchell Lisle <mitchelllisle@mailbox.org>
2023-03-31 21:53:07 +1100

    add project test suite back in

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/logger.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/logger.py
@@ -2,4 +2,4 @@ import os
 import logging
 
 logging.basicConfig(level=os.getenv("LOG_LEVEL", "INFO").upper())
-logger = logging.getLogger("{{cookiecutter.full_name}}")
+logger = logging.getLogger("{{cookiecutter.project_slug}}")
