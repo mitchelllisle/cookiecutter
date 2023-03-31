c195c1530b7ef3040586cb9672b47f69dfaaa19d
Mitchell Lisle <mitchelllisle@mailbox.org>
2023-03-31 12:07:25 +1100

    change generated project to poetry

--- tests/test_default_proj_generate.py
+++ tests/test_default_proj_generate.py
@@ -13,12 +13,8 @@ def test_default_project(cookiecutter_config, expected_folders, testing_dir):
 
     directory_contents = glob(f'{testing_dir}/{project_folder[0]}/*', recursive=True)
 
-    assert all(
-        [
-            f.replace(f'{testing_dir}/{project_folder[0]}/', '') in expected_folders
-            for f in directory_contents
-        ]
-    )
+    for f in directory_contents:
+        assert f.replace(f'{testing_dir}/{project_folder[0]}/', '') in expected_folders
 
 
 def test_docs_are_ommited(cookiecutter_config, expected_folders, testing_dir):
@@ -33,3 +29,25 @@ def test_docs_are_ommited(cookiecutter_config, expected_folders, testing_dir):
     directory_contents = glob(f'{testing_dir}/{project_folder[0]}/*', recursive=True)
 
     assert f'{testing_dir}/{project_folder[0]}/docs' not in directory_contents
+
+
+def test_entrypoint(cookiecutter_config, expected_folders, testing_dir):
+    main.cookiecutter(
+        '.', no_input=True, output_dir=testing_dir, extra_context={'entrypoint': True}
+    )
+    assert utils.make_sure_path_exists(testing_dir) is True
+
+    with open(f'{testing_dir}/my_project/pyproject.toml') as f:
+        text = f.read()
+        assert 'my_project = { callable = "my_project.__main__:main"}' in text
+
+
+def test_no_entrypoint(cookiecutter_config, expected_folders, testing_dir):
+    main.cookiecutter(
+        '.', no_input=True, output_dir=testing_dir, extra_context={'entrypoint': False}
+    )
+    assert utils.make_sure_path_exists(testing_dir) is True
+
+    with open(f'{testing_dir}/my_project/pyproject.toml') as f:
+        text = f.read()
+        assert 'my_project = { callable = "my_project.__main__:main"}' not in text
